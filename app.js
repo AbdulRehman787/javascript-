a2ff8fa036d8f34c86c5db5448dcbc1d4270955b
AbdulRehman787 <88674059+AbdulRehman787@users.noreply.github.com>
2022-01-20 19:33:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,557 @@
+//alert("Hello World");
+
+
+// var a = "Hello Mr bean \n How r you";
+// alert(a)
+
+
+// var a = "Hello World"
+//console.log(a)
+
+
+// var book = "A smarter way to learn JavaScript.";
+// alert(book)
+
+// document.write("A smarter way to learn JavaScript")
+
+
+// var age = 18;
+// console.log(18)
+
+// var birthYear = "30 06 2003";
+// console.log(birthYear)
+
+// var a = "Abdul ";
+// var b = "Rehman";
+// var c = a+b;
+// console.log(c) 
+
+// var $game = "Hello World";
+// console.log($game)
+
+// var a  = 5;
+// var b = 3;
+// var c= a+b;
+// console.log("Sum of 3 + 5 is equal to " +c)
+
+
+
+// var a  = 5;
+// var b = 3;
+// var c= a-b;
+// console.log("Sum of 5 - 3 is equal to " +c)
+
+
+// var a  = 5;
+// var b = 3;
+// var c= a*b;
+// console.log("Sum of 5 * 3 is equal to " +c)
+
+
+
+// var a  = 10;
+// var b = 2;
+// var c= a/b;
+// console.log("Sum of 10 / 2 is equal to " +c)
+
+// var a = 5;
+// var b = a + ++a -  a-- - --a
+// console.log(b)
+
+
+// var tictekPrice = 600;
+// var buyer = 5;
+// var finalTicktetPrice = tictekPrice* buyer;
+// console.log("Total sum of buyer ticket 5 * 600 is equalprice " +finalTicktetPrice  )
+
+
+// document.write(4 + " " + "x" + " 1" + " =" +" 4" + "<br>" )
+// document.write(4 + " " + "x" + " 2" + " =" +" 8"  + "<br>")
+// document.write(4 + " " + "x" + " 3" + " =" +" 12"  + "<br>")
+// document.write(4 + " " + "x" + " 4" + " =" +" 16"  + "<br>")
+// document.write(4 + " " + "x" + " 5" + " =" +" 20"  + "<br>")
+// document.write(4 + " " + "x" + " 6" + " =" +" 24"  + "<br>")
+// document.write(4 + " " + "x" + " 7" + " =" +" 28"  + "<br>")
+// document.write(4 + " " + "x" + " 8" + " =" +" 32"  + "<br>")
+// document.write(4 + " " + "x" + " 9" + " =" +" 36"  + "<br>")
+// document.write(4 + " " + "x" + " 10" + " =" +" 40"  + "<br>")
+
+
+// var priceOfItem1 = 650;
+// var quantity1 = 3;
+// var priceOfItem2 = 100;
+// var quantity2  = 7;
+// var price1 = ((priceOfItem1* quantity1) + (priceOfItem2 *quantity2) +100)
+// console.log(price1  )
+
+// var totalMark = 980;
+// var obtMark = 804;
+// var percentage = obtMark / totalMark *100;
+// console.log(percentage)
+
+
+// var usdollar = 104.80;
+// var price =10;
+// var resultdollar = (usdollar*price);
+// var riyal  = 28;
+// var pricer = 25;
+// var resultRiyal = (riyal *pricer)
+// var total = resultRiyal +resultdollar;
+// console.log("Total currency in pkr is" +total)
+
+
+// var currentYear = 2022;
+// var birthYear = 2003;
+// var final = currentYear-birthYear;
+// console.log(final)
+
+
+// var a = prompt("Enter a your name")
+// console.log(a)`
+
+
+// var table = prompt("Enter your table  ")
+// for(var i=1;i<=10;i++){
+//     console.log(table + " " + 'x' + i + "=" +table*i )
+
+// }
+
+
+
+// var a = prompt('Enter a city Name');
+// if(a==="Karachi"){
+//     console.log("Welcome to Karachi Jani")
+// }else{
+//     console.log("Karachi aja phir dekhty hy ")
+// }
+
+// var gender = prompt("Enter your gender ")
+// if(gender === "Male"){
+//     alert("Good Morning Sir!,")
+// }else if(gender === "Female"){
+//     alert("Good Morning Maam!")
+// }else{
+//     alert("Ok you are gay")
+// }
+
+// var signal = prompt("Enter a signal Color")
+// if(signal === "Red"){
+//     console.log("Must STop !>")
+// } else if(signal === 'Green'){
+//     console.log("Ready to goo")
+// }else if(signal === 'Yellow'){
+//     console.log("Ready to move")
+// }
+
+// var fuel = prompt("Enter a remaining fuel in your car")
+// if(fuel ==="0.25"){
+//     console.log("Bhai petrol pump ja ka fuel dala bsdk ")
+// }else{
+//     console.log("fuel car")
+// }
+
+// if("car" < "cat"){
+//     console.log("Car is smaller than cat    ")
+//  }
+
+// var user = prompt("Enter a anyone number")
+// var guess = 2;
+// if(user == guess ){
+//     console.log('You are Win ')
+// }else{
+//     console.log("You are loss try Again")
+// }
+
+// var table = prompt("Enter a number this is divisble by 3 ")
+// if(table%3===0){
+//     console.log(table+" this number is divesble by 3")
+// }else{
+//     console.log(table+" this number is not  divesble by 3")
+// }
+
+// var number= prompt("Enter a number check thsi number is even number")
+// if(number%2===0){
+//     console.log("This is even Number")
+// }else{
+//     console.log("This is odd Number ")
+// }
+
+// var user = prompt("Enter a your cute temperature")
+// if(user>=30){
+//     console.log("Today is hot day")
+
+// }else if(user>=20){
+//     console.log("Today is normal day")
+// }else if(user>10){
+//     console.log("today is cool day ")
+// }else if(user>0){
+//     console.log('Omg today weatheris cool day ')
+//}
+// var user1 = +prompt("Enter the first number you want calculation with other number")
+// var sign = prompt("Enter a sign value ex.    +  -")
+// var user2 = +prompt("Enter the second number you want calculation with other number")
+
+// if(sign === "+"){
+//     console.log(user1+user2)
+// }else if(sign=== "-"){
+//     console.log(user1-user2)
+// }else if(sign=== "*"){
+//     console.log(user1*user2)
+// }else if(sign=== "/"){
+//     console.log(user1/user2)
+// }else if(sign=== "%"){
+//     console.log(user1%user2)
+// }
+
+// var user1 = prompt("Enter a alphabate check this is lower case to upper case")
+// if(user1==="A" && user1==="Z"){
+//     console.log("A = 65 ")
+// // }
+
+// var first = prompt("Enteer th first integar")
+// var second =prompt("Enter the second integar")
+// if(first === second){
+//     console.log("first integar is equal to second integar")
+// }else{
+//     console.log("first integar is not equal to second integar")
+// }
+
+// var user = prompt("enter the number check this is positive negative then zero")
+// if(user>0){
+//     console.log("this is positive number")
+// }else if(user<0){
+//     console.log("This is negative number")
+// }else {
+//     console.log("This is zero number")
+// // }
+
+// var user = prompt("Enter a anyone  vovel alphabate ")
+// if(user === "A" || user === "E" || user === "I" || user === "O" || user === "U"|| user === "a" || user === "e" || user === "i"||user === "o" || user === "u"){
+// console.log("This is Vovel ")
+// }else {
+//     console.log(" Sorry ,This is not vovel")
+// }
+
+// var password = prompt("Enter a password")
+// var check = "A.Rehman";
+// if(check === password){
+//     console.log("This is correct Password")
+// } else{
+//     console.log("Incorrect Password")
+// }
+
+// var hour = 13;
+// if(hour < 18) {
+//     greeting = "Good day";
+// }
+// else{
+//     greeting = "Good evening";
+// }
+
+// var arr= [];
+// arr[0]="Abdul",
+// arr[1]="Rehman",
+// arr[2]="Aslam"
+// console.log(arr)
+
+// var arr = ["Abdul","Rehman","Aslam"]
+// console.log(arr)
+
+// var  arr=[0,102,203,40,450,560,7,88053,0303,303 ]
+// console.log(arr)
+
+
+
+// var  arr=[0,102,"Aslam  ",203,40,450,560,7,"aNBSYDV", "Abdul Rehman",0303,303 ]
+// console.log(arr)
+
+
+// var arr = ["Matric","FirstYear",'SecondYear',"Graguation","Master","Phd"]
+// console.log(arr)
+
+// var cities = ["Karachi","Lahore","Islamabad","Multan","Quetta"]
+// cities=cities.splice(2,2)
+// console.log(cities)
+
+// var arr = ["This ","is  ","my ","cat"]
+// console.log(arr)
+// arr=arr.join()
+// console.log(arr)
+
+// var arr = ["Mouse","Keyborad","Laptop","Mobile","charge"]
+// arr.pop()
+// arr.pop()
+// arr.pop()
+// console.log(arr)
+
+// var arr = [
+//     [0, 1, 2, 3],
+//     [1, 0, 1, 2],
+//     [2, 1, 0, 1]
+// ];
+// for(var i=0; i<3; i++){
+//     for(let j=0 ;j<4 ;j++){
+//         document.write(arr[i][i]+ " ")
+//     }
+//     document.write("</br>");
+// }
+
+// var a = [
+//     [0, 1, 2, 3],
+//     [1, 0, 1, 2],
+//     [2, 1, 0, 1]
+// ];
+// for (var i = 0; i < 3; i++) {
+//     for (let j = 0; j < 4; j++) {
+//         document.write(a[i][j]+" ");
+//     }
+//      document.write("</br>");
+// }
+
+// for(var i=1;i<=10;i++){
+//     console.log(i)
+// }
+
+// var table = prompt("Enter a table number");
+// var user = +prompt("Enter the length of number you want")
+// for(var i =1;i<=100;i++){
+//     for(var j=i;j<=user;j++){
+//         console.log(table + " " + "x" + i + " =" + table*i)
+//     }
+// }
+
+// var arr =["Stawberry","banana","orange","mango"]
+
+// for(var i=1;i<=10;i++){
+  //  console.log(i)
+//}
+
+//  for(var j=10;j>=1;j--){
+//     console.log(j)
+// // }
+// var z = +prompt("Enter a number")
+// for(var i=0;i%2;i++){
+//   if(z === i ){
+//     console.log("Even Number")
+//   }
+    
+// }
+
+  // for(var i=1;i<=20;i++){
+  //   console.log(i)
+  // }
+
+
+// for(var i=1;i<=20;i=i+2){
+//   console.log(i)
+// }
+
+
+// for(var i=0;i<=20;i=i+2){
+//   console.log(i+ "k")
+// }
+
+
+// for(var i=1;i<=20;i=i+2){
+//   console.log(i)
+// }
+
+// var arr =[0,8,2,34,5]
+// console.log(Math.max(...arr))
+
+
+// var arr =[0,8,2,34,5]
+// console.log(Math.min(...arr))
+
+// var arr = [9,676,5345,345]
+// console.log(Math.max(...arr))
+
+// var arr = [9,676,5345,345]
+// console.log(Math.min(...arr))
+
+// for(var i=5;i*5;i++){
+//   console.log(i)
+//}
+
+// var firstName = prompt("Enter a first Name ")
+// var LastName = prompt("Enter a Last Name")
+// var full = firstName+ " "  + LastName;
+// console.log("Greeting " + full)
+
+
+// var fvr = prompt('Enter your favorite mobile model name')
+// fvr= fvr.length;
+// console.log(fvr)
+
+// var nam = "Pakistani";
+// nam=nam.indexOf("n")
+// console.log(nam)
+
+// var indux= "Hello World"
+// indux=indux.lastIndexOf("l")
+// console.log(indux)
+
+
+// var nam = "Pakistani";
+// nam=nam.indexOf("i")
+// console.log(nam)
+
+// var firstName = prompt("Enter a first Name ")
+// var LastName = prompt("Enter a Last Name")
+// var full = firstName+ " "  + LastName;
+// console.log("Greeting " + full)
+
+// var city="Hydera bad"
+// var ind= city.indexOf("Hydera");
+// var first = city.slice(0,ind);
+// var replace= "Islama";
+// var third = city.slice(ind +6);
+// console.log(first+ replace  +third)
+
+
+// var text = "HyderaBad";
+// var indxnum = text.indexOf("Hydera")
+// var first = text.slice(0,indxnum)
+// var replace = "Islama"
+// var third=text.slice(indxnum+6)
+// console.log(first+replace+third)
+
+// var text   ="Sammer and ali are best friend  we both play a games"
+// var ind = text.indexOf("and")
+
+// var first = text.slice(0,ind)
+// var replace = "&"
+// var third = text.slice(ind +3)
+// console.log(first+replace+third)
+
+// var user ="peanut"
+// user=user.toLocaleUpperCase()
+// console.log(user)
+
+
+// var user ="peanuts"
+// user=user.toLowerCase();
+// console.log(user)
+
+// var num = 65.56;
+// if(num ===" ."){
+//   console.log(num)
+// }
+
+// var checked = "65.56"
+// checkedNew = checked.split('.').join("");
+// console.log(checkedNew);
+
+// var num = "34.35"
+// checknum = num.split('.').join("")
+// console.log(checknum)
+
+// var university = "University of Karachi"
+// var arr = university;
+// check = arr.split();
+// console.log(check)  
+
+
+// var num = 65.56;;
+// num = num.toString();
+// console.log(num)
+// var num = 1.0 
+// var round = Math.ceil(num); 
+// console.log(round)  
+
+// var user = +prompt("Enter a number ");
+// document.write("<div>","Number: "+user+"</div>");
+// document.write("<div>","Round off value: "+Math.round(user)+"</div>");
+// document.write("<div>","Floor off value: "+Math.floor(user)+"</div>");
+// document.write("<div>","Ceil off value: "+Math.ceil(user)+"</div>");
+
+
+
+// var user = prompt("Enter a number ");
+// document.write("<div>","Number: "+user+"</div>");
+// document.write("<div>","Round off value: "+Math.round(user)+"</div>");
+// document.write("<div>","Floor off value: "+Math.floor(user)+"</div>");
+// document.write("<div>","Ceil off value: "+Math.ceil(user)+"</div>");
+
+// var user = prompt("Enter a any integar")
+// document.write("<div>","the absolute value is ",Math.abs(user),"</div>")
+
+
+// var headuser = prompt("Enter head user Name")
+// var tailuser = prompt("Enter tail user name")
+// var toss = Math.random()*2;
+// if(toss===0){
+//   console.log( "Head"+headuser  + "Won the Toss ")
+// }else{
+//   console.log("Tail "+tailuser  + "Won the toss")
+// }
+
+// var diec = Math.round( Math.random()*6);
+// if(diec ===1){
+//   Math.round=1
+// }
+// document.write("random diec value is",diec)
+
+
+
+// var randomNum = Math.round(Math.random() * 100);
+// if(randomNum===0){
+//     randomNum = 1;
+// }
+// document.write("<div>","Random number value between 1 to 100: " +randomNum+"</div>");
+
+// var randomNum = Math.round(Math.random()*100);
+// if(randomNum ===0){
+//   randomNum=1
+// }
+// document.write("<div>"+ "Random number is between 1 to 100 "+randomNum+ "</div>")
+
+
+// var abc= prompt("Enter Your Weight");
+// var xyz= parseFloat(abc);
+// document.write("<div>","The weight of user is: " +xyz+" kilograms"+"</div>");
+
+// var abc = prompt("Enter your Weight")
+// var xyz = parseFloat(abc)
+// document.write("Your Weight is " + abc)
+
+// var user = +prompt("Enter number between 1 to 10")
+// var randomNum = Math.round(Math.random() * 10);
+// if(randomNum===0){
+//     randomNum = 1;
+// }
+
+// else if(randomNum===user){
+//     alert("Congratulation!! You have enter correct secret number ");
+// }
+// else{
+//     alert("Sorry !! Better luck next time");
+    
+// }
+
+
+// var date = new Date();
+// console.log(date)
+// var day = ["Janurary ","Feburary","March","April","May","June","July","Agust","September","November","December"]
+// console.log(day[date.getMonth()])
+
+
+// var date = new Date();
+// var user = prompt("Enter a integar")
+// var day = ["Sun","Mon","Tue","Wed","Thr","fri","sat"]
+// console.log(day[date.getDay()])
+
+// function sum (a,b){
+//   var total =a+b
+//   console.log(total)
+// }
+// sum()
+// sum(30,80)
+// sum(20,90)
+
+function sum (a,b){
+  return totat =a+b;
+
+}
+var funexp = sum(5,4)
+console.log("The sum of number is",funexp)
